0d5e44a679b755293db7033f805d8138e58592b9
Steven <e0390018@u.nus.edu>
2022-02-22 18:48:00 +0800

    pratice

---
+++ 02/01/GlobalUsings.cs
@@ -0,0 +1,4 @@
+// will not be add to 01.GlobalUsings.g.cs
+global using System.Reflection;
+global using System.Linq;
+global using System;
---
+++ 02/01/Program.cs
@@ -0,0 +1,11 @@
+﻿Assembly? entryAssembly = Assembly.GetEntryAssembly();
+if (entryAssembly == null) return;
+
+foreach(AssemblyName assemblyName in entryAssembly.GetReferencedAssemblies()){
+    Assembly assembly = Assembly.Load(assemblyName);
+    int methodCount = 0;
+    foreach (TypeInfo typeInfo in assembly.DefinedTypes){
+        methodCount += typeInfo.GetMethods().Count();
+    }
+    Console.WriteLine($"{assembly.DefinedTypes.Count()} types with {methodCount} methods in {assemblyName} assembly.");
+}
